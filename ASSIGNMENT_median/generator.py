777ff5260f6f6c044b8b193ef5fbf63833256cbb
giovanni <giovanni.mazzocchin@studenti.unipd.it>
2018-08-20 16:12:01 +0200

    add generator

---
+++ ASSIGNMENT_median/generator.py
@@ -0,0 +1,17 @@
+import random
+import math
+
+exp = 6
+union = random.sample(range(1, 5000), int((math.pow(2, exp))))
+party_1 = union[0:(int(math.pow(2, (exp-1))) - 1)]
+party_2 = union[int(math.pow(2, (exp-1))):(int(math.pow(2, exp)) - 1)]
+
+union.sort()
+known_median = union[int(math.pow(2, exp-1)) - 1]
+
+print party_1
+print party_2
+print known_median
+
+
+
