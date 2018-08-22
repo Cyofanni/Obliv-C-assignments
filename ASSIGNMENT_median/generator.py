dc15ca143ab5b78e2927f7b547bf78b398ff50c9
giovanni <giovanni.mazzocchin@studenti.unipd.it>
2018-08-22 18:09:06 +0200

    add generator and test script

--- ASSIGNMENT_median/generator.py
+++ ASSIGNMENT_median/generator.py
@@ -1,10 +1,12 @@
 import random
 import math
 
-exp = 6
-union = random.sample(range(1, 5000), int((math.pow(2, exp))))
-party_1 = union[0:(int(math.pow(2, (exp-1))) - 1)]
-party_2 = union[int(math.pow(2, (exp-1))):(int(math.pow(2, exp)) - 1)]
+exp = 12
+union = random.sample(range(1, 6000), int((math.pow(2, exp))))
+print len(union)
+#print union
+party_1 = union[0:(int(math.pow(2, (exp-1))))]
+party_2 = union[int(math.pow(2, (exp-1))):(int(math.pow(2, exp)))]
 
 union.sort()
 known_median = union[int(math.pow(2, exp-1)) - 1]
@@ -12,3 +14,6 @@ known_median = union[int(math.pow(2, exp-1)) - 1]
 print party_1
 print party_2
 print known_median
+
+
+
