9d0aa785ef2814046b5c2be64ffba761bc1a03bb
Giovanni Mazzocchin <giovanni.mazzocchin@studenti.unipd.it>
2018-08-22 15:47:49 +0200

    Update generator.py

--- ASSIGNMENT_median/generator.py
+++ ASSIGNMENT_median/generator.py
@@ -3,8 +3,8 @@ import math
 
 exp = 6
 union = random.sample(range(1, 5000), int((math.pow(2, exp))))
-party_1 = union[0:(int(math.pow(2, (exp-1))) - 1)]
-party_2 = union[int(math.pow(2, (exp-1))):(int(math.pow(2, exp)) - 1)]
+party_1 = union[0:(int(math.pow(2, (exp-1))))]
+party_2 = union[int(math.pow(2, (exp-1))):(int(math.pow(2, exp)))]
 
 union.sort()
 known_median = union[int(math.pow(2, exp-1)) - 1]
